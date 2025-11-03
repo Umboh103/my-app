d95bc90c1b57b50650932b635f5e9cc58472646d
Umboh103 <s22310184@student.unklab.ac.id>
2025-11-03 21:50:33 +0800

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header/Header";
-
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-4 space-y-4">
       <Header title="Test" />
+      <Home title="Welcome to Pokedex" />
     </div>
   );
 }
