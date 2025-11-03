116322b54005e99527d0a0a1c32362efd57b559e
stivo12-k <kunangkunang139@gmail.com>
2025-11-03 22:40:25 +0800

    stivo/FooterCardInfo

---
+++ src/components/Home/CardInfo/TypeEffect.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TypeEffect = ({ info }) => {
+  return (
+    <div className="bg-yellow-100 p-2 rounded-lg w-1/3 text-center">
+      {info}
+    </div>
+  );
+};
+
+export default TypeEffect;
